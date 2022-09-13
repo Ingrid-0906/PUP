f466796cdf17f35d37ab254e3a4d77a8ad590877
Ingrid_Cadu <92744210+Ingrid-0906@users.noreply.github.com>
2022-09-13 00:51:37 -0300

    Update datasource.py

--- datasource.py
+++ datasource.py
@@ -38,17 +38,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Opening data and taking the columns i need to proceed
-url_source = 'https://github.com/Ingrid-0906/Obiwan_2022/blob/3e58574016524eb88578322898e063aede8b9fae/CSVFiles/Original_grocery_280k.csv'
+url_source = 'https://media.githubusercontent.com/media/Ingrid-0906/Obiwan_2022/main/CSVFiles/Original_grocery_280k.csv'
 df = pd.read_csv(url_source)
 df = df.loc[:,['title','category']]
 
 # Opening data and taking the columns i need to proceed
-url_feed = 'https://github.com/Ingrid-0906/Obiwan_2022/blob/3e58574016524eb88578322898e063aede8b9fae/CSVFiles/metadata_Feeding_GROCERIES.csv'
+url_feed = 'https://media.githubusercontent.com/media/Ingrid-0906/Obiwan_2022/main/CSVFiles/metadata_Feeding_GROCERIES.csv'
 feed = pd.read_csv(url_feed)
-feed[:2]
+
 
 """# Working with categories - Cleaning phase"""
-
 # Splititng the categories into columns
 def get_columns(categ):
   try:
@@ -58,8 +57,8 @@ def get_columns(categ):
   except TypeError: # Error if the outer list is missing
     return 'no_category'
 
-"""## Original data"""
 
+"""## Original data"""
 # Create column for category path
 df['category_path'] = df['category'].apply(get_columns)
 
@@ -94,10 +93,9 @@ df_1 = df_1[~df_1[2].isin(counts[counts < 11].index)]
 # Reseting index for all dataframe
 df_1 = df_1.reset_index()
 df_1 = df_1.loc[:,['title',1,2]]
-df_1.head(2)
+
 
 """## Feeding data"""
-
 # Create column for category path
 feed['category_path'] = feed['category'].apply(get_columns)
 
@@ -114,7 +112,6 @@ feed_1 = feed_1.dropna(subset=['title'], axis=0)
 
 feed_1 = feed_1.loc[:,['title',0,1]]
 feed_1 = feed_1.rename(columns={0:1, 1:2})
-feed_1[:2]
 
 # Linking both datas
 df_1 = pd.concat([df_1,feed_1])
